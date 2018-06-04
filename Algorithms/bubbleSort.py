d8dcb7aab36b6061440b4324942db15caebc42a8
JDSeiler <33209724+JDSeiler@users.noreply.github.com>
2018-06-04 11:26:59 -0500

    Added bubble sort

---
+++ Algorithms/bubbleSort.py
@@ -0,0 +1,21 @@
+﻿'''bubble sort in python'''
+
+def bubble(array):
+	sort = False
+	while sort == False:
+		swap = 0
+		for num in array:
+			if array.index(num) == len(array) - 1:
+				pass
+			elif array[array.index(num)] > array[array.index(num) + 1]:
+				theIndex = array.index(num)
+				theBubble = array.pop(theIndex)
+				array.insert(theIndex + 1, theBubble)
+				swap += 1
+			else:
+				pass
+		if swap == 0:
+			sort = True
+			return array
+
+
