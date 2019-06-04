13483d6828ba4169ea488f6e450b9b136a488d31
JDSeiler <jorsei1p217@gmail.com>
2019-06-04 11:37:00 -0400

    Add method to populate Plane object with random points

--- closest-pair-2d/plane.py
+++ closest-pair-2d/plane.py
@@ -13,10 +13,18 @@ class Plane:
         """
         self.points = []
 
-    def add_points(self, new_points: typing.List[Point]):
-        """Add a list of Point objects to the plane"""
-        self.points.append(new_points)
+    def generate_random_plane(self, N: int, span: int):
+        """Populates the plane with N random points, where span is the maximum value a point
+        could have for either its x or y value. For instance, if the span is 5, the x and y value
+        of every point will be between -5 and 5.
+        """
+        rnd.seed()
+        for i in range(0, N):
+            randx = float(rnd.randrange(-span, span))
+            randy = float(rnd.randrange(-span, span))
+            new_point = Point(randx, randy)
+            self.points.append(new_point)
 
-    
+
     
     
